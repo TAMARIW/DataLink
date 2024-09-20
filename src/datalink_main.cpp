daa6c7edd220d143e7d42b08c41515d37bd6d9a4
chrissteffen98 <chrissteffen98@gmail.com>
2024-09-20 09:33:09 +0200

    Changed wifi enable to simply turn on wifi and not setup

--- src/datalink_main.cpp
+++ src/datalink_main.cpp
@@ -77,8 +77,9 @@ void datalinkWiFiConnectFunc(bool& enable) {
 
     if (enable) {
         PRINTF("Enabling wifi connect\n");
-        std::system("sudo iwconfig wlan0 mode ad-hoc essid TMWNetwork key off");
-        std::system("sudo ifconfig wlan0 192.168.10.1 netmask 255.255.255.0 up");
+        //std::system("sudo iwconfig wlan0 mode ad-hoc essid TMWNetwork key off");
+        //std::system("sudo ifconfig wlan0 192.168.10.1 netmask 255.255.255.0 up");
+        std::system("sudo ifconfig wlan0 up");
     } else {
         PRINTF("Disabling wifi connect\n");
         std::system("sudo ifconfig wlan0 down");
